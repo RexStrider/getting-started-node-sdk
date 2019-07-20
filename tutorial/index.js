914747bbf63dd2d567a94d601f3fc4b69f86b46e
Odysseus <arwaterman@gmail.com>
2019-07-20 12:31:46 -0700

    updated tutorial to es6 Javascript

--- tutorial/index.js
+++ tutorial/index.js
@@ -20,49 +20,26 @@ const client = new smartcar.AuthClient({
 // global variable to save our accessToken
 let access;
 
-app.get('/login', function(req, res) {
+app.get('/login', (req, res) => {
   // TODO: Authorization Step 1b: Launch Smartcar authentication dialog
   const link = client.getAuthUrl();
   res.redirect(link);
 });
 
-app.get('/exchange', function(req, res) {
+app.get('/exchange', async (req, res) => {
   // TODO: Authorization Step 3: Handle Smartcar response
   const code = req.query.code;
-  // console.log(code);
-  // res.sendStatus(200);
-  // TODO: Request Step 1: Obtain an access token
-  return client.exchangeCode(code)
-         .then(function(_access) {
-           // in a production app you'll want to store this in some kind of persistent storage
-           // access token is valid for 2 hours
-           access = _access;
-           res.sendStatus(200);
-         });
+  access = await client.exchangeCode(code);
+  res.sendStatus(200);
 });
 
-app.get('/vehicle', function(req, res) {
-  // TODO: Request Step 2: Get vehicle ids
-  return smartcar.getVehicleIds(access.accessToken)
-         .then(function(data) {
-           return data.vehicles; // list of vehicle ids
-         })
-  // TODO: Request Step 3: Create a vehicle
-         .then(function(vehicleIds) {
-           const vehicle = new smartcar.Vehicle(vehicleIds[0], access.accessToken);
-           return vehicle.info();
-         })
-  // TODO: Request Step 4: Make a request to Smartcar API
-         .then(function(info) {
-           console.log(info);
-           // {
-           //   id,
-           //   make,
-           //   model,
-           //   year
-           //  }
-           res.json(info);
-         })
+app.get('/vehicle', async (req, res) => {
+  const data = await smartcar.getVehicleIds(access.accessToken);
+  const vehicleIds = await data.vehicles;
+  const vehicle = new smartcar.Vehicle(vehicleIds[0], access.accessToken);
+  const info = await vehicle.info();
+  console.log(info);
+  res.json(info);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
